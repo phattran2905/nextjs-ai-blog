28817815a4cd905711c8af0948910df120412a96
phat@minim1 <phat.tranducvn@gmail.com>
2023-07-13 20:03:05 -0700

    Add Nav component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
+import Navbar from "app/(shared)/Navbar";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
@@ -15,7 +16,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 			lang="en"
 			className={openSans.className}
 		>
-			<body>{children}</body>
+			<body>
+				<Navbar />
+				{children}
+			</body>
 		</html>
 	);
 }
