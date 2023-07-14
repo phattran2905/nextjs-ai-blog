644ab367f1c498006ab5a0478d7c18a41ab57feb
phat@minim1 <phat.tranducvn@gmail.com>
2023-07-13 20:17:18 -0700

    Add social links

---
+++ app/(shared)/SocialLinks.tsx
@@ -0,0 +1,95 @@
+import Image from "next/image";
+import Twitter from "public/assets/social_twitter.png";
+import Facebook from "public/assets/social_facebook.png";
+import Google from "public/assets/social_google.png";
+import Instagram from "public/assets/social_instagram.png";
+import Pinterest from "public/assets/social_pinterest.png";
+import Discord from "public/assets/social_discord.png";
+
+type Props = {
+	isDark?: boolean;
+};
+export default function SocialLinks({ isDark }: Props) {
+	return (
+		<div className="flex justify-between items-center gap-7">
+			<a
+				href="https://twitter.com"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<Image
+					className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
+					alt="twitter"
+					src={Twitter}
+					width={20}
+					height={20}
+				/>
+			</a>
+			<a
+				href="https://facebook.com"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<Image
+					className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
+					alt="facebook"
+					src={Facebook}
+					width={20}
+					height={20}
+				/>
+			</a>
+			<a
+				href="https://google.com"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<Image
+					className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
+					alt="Google"
+					src={Google}
+					width={20}
+					height={20}
+				/>
+			</a>
+			<a
+				href="https://instagram.com"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<Image
+					className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
+					alt="instagram"
+					src={Instagram}
+					width={20}
+					height={20}
+				/>
+			</a>
+			{/* <a
+				href="https://pinterest.com"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<Image
+					className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
+					alt="pinterest"
+					src={Pinterest}
+					width={20}
+					height={20}
+				/>
+			</a> */}
+			<a
+				href="https://discord.com"
+				target="_blank"
+				rel="noreferrer"
+			>
+				<Image
+					className={`${isDark ? "brightness-0" : ""} hover:opacity-50`}
+					alt="discord"
+					src={Discord}
+					width={20}
+					height={20}
+				/>
+			</a>
+		</div>
+	);
+}
