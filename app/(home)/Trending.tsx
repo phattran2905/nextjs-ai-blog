f1f4f442419a2b1e93b9269ee9627ee1562caaf4
phat@minim1 <phat.tranducvn@gmail.com>
2023-07-14 15:26:02 -0700

    Add Trending

---
+++ app/(home)/Trending.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+
+type TrendingCardProps = {
+	className?: string;
+};
+
+const TrendingCard = ({ className }: TrendingCardProps) => {
+	return (
+		<Link
+			href="/"
+			// href={`${process.env.NEXT_PUBLIC_URL}/post/${post?.id}`}
+			className={`${className} sm:mt-0 sm:h-auto relative mt-7 block w-full h-96 hover:opacity-70`}
+		>
+			<div className="z-0 relative w-full h-full bg-wh-500">image</div>
+			<div className="absolute z-1 top-0 left-0 w-full h-full bg-gradient-gradual"></div>
+			<div className="absolute z-2 bottom-0 left-0 p-3">
+				<h4 className="inline-block px-5 py-1 font-semibold bg-accent-orange text-wh-900">
+					category
+				</h4>
+				<p className="text-wh-100 mt-2">post title</p>
+			</div>
+		</Link>
+	);
+};
+
+type Props = {};
+
+export default function Trending({}: Props) {
+	return (
+		<section className="pt-3 pb-10">
+			<div className="flex items-center gap-3">
+				<div className="bg-wh-900 py-2 px-8 text-wh-10 text-sm font-bold ">Trending</div>
+				<p className="text-sm">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolorem voluptates
+					placeat delectus ipsum. Hic?
+				</p>
+			</div>
+
+			<div className="sm:grid gap-5 grid-cols-4 grid-rows-2 sm:h-[600px] my-3">
+				<TrendingCard className="col-span-2 row-span-2 bg-wh-500" />
+				<TrendingCard className="col-span-2 row-span-1 bg-wh-500" />
+				<TrendingCard className="col-span-1 row-span-1 bg-wh-500" />
+				<TrendingCard className="col-span-1 row-span-1 bg-wh-500" />
+			</div>
+		</section>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
 import Image from "next/image";
+import Trending from "./(home)/Trending";
 
 export default function Home() {
-	return <main className="">hi</main>;
+	return (
+		<main className="px-10 leading-7">
+			<Trending />
+			<div className="md:flex gap-10 mb-5">
+				<div className="basis-3/4">
+					<div>Tech</div>
+					<div>Trave;</div>
+					<div>Other</div>
+					<div className="hidden md:block">
+						<div>CTA Subcribe</div>
+					</div>
+				</div>
+				<div className="basis-1/4">{/* Sidebar */}a</div>
+			</div>
+		</main>
+	);
 }
