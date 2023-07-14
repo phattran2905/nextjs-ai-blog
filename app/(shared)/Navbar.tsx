72ad568112ebe210bc2dc9b3007ef23910d1d862
phat@minim1 <phat.tranducvn@gmail.com>
2023-07-13 20:17:29 -0700

    Add Navbar

--- app/(shared)/Navbar.tsx
+++ app/(shared)/Navbar.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
+import SocialLinks from "./SocialLinks";
 
 type Props = {};
 export default function Navbar({}: Props) {
 	return (
 		<header className="mb-5">
 			<nav className="flex justify-between items-center w-full bg-wh-900 text-wh-10 px-10 py-4">
-				<div className="hidden sm:block">social link</div>
+				<div className="hidden sm:block">
+					<SocialLinks />
+				</div>
 				<div className="flex justify-between items-center gap-10">
 					<Link href={"/"}>Home</Link>
 					<Link href={"/"}>Trending</Link>
